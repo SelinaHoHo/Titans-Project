88a77dea62713e10dd8f0f046cf196b5ea1ad898
Selina <selina@Quys-MacBook-Pro.local>
2024-03-22 09:32:12 +0700

    feat(TSP-13): create Get in touch section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,16 @@
 module.exports = {
   content: ["./client/src/**/*.{html,js}"],
   theme: {
-    extend: {},
-    screens: {
-      sm: { max: "479px" },
-      md: { max: "767px" },
-      lg: { max: "1023px" },
-    },
-    fontFamily: {
-      open: ["Open Sans", "sans-serif"],
-      serif: ["Raleway", "serif"],
-    },
     extend: {
+      screens: {
+        sm: { max: "479px" },
+        md: { max: "767px" },
+        lg: { max: "1023px" },
+      },
+      fontFamily: {
+        open: ["Open Sans", "sans-serif"],
+        serif: ["Raleway", "serif"],
+      },
       spacing: {
         128: "32rem",
         144: "36rem",
@@ -20,11 +19,15 @@ module.exports = {
       borderRadius: {
         "4xl": "2rem",
       },
-    },
-    colors: {
-      beMain: "#FDF6EA",
-      beLight: "#F5E9D4",
-      xanhMain: "#838C48",
+
+      colors: {
+        beMain: "#FDF6EA",
+        beLight: "#F5E9D4",
+        xanhMain: "#838C48",
+      },
+      backgroundImage: {
+        inTouch: "url('../images/newsletter-parallax.png')",
+      },
     },
   },
   plugins: [],
