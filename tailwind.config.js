e5c377e553482f9e092cdeb977c0ffcbd0b7f74d
KhanhNguyen2710 <hanaaimi304@gmail.com>
2024-03-24 14:37:48 +0700

    feat(TSP-9): header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,30 +2,32 @@
 module.exports = {
   content: ["./client/src/**/*.{html,js}"],
   theme: {
-    extend: {},
-    screens: {
-      sm: "480px",
-      md: "768px",
-      lg: "1024px",
-    },
-    fontFamily: {
-      open: ["Open Sans", "sans-serif"],
-      serif: ["Raleway", "serif"],
-    },
     extend: {
-      spacing: {
-        128: "32rem",
-        144: "36rem",
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "1024px",
       },
-      borderRadius: {
-        "4xl": "2rem",
+      fontFamily: {
+        open: ["Open Sans", "sans-serif"],
+        serif: ["Raleway", "serif"],
+      },
+   
+        spacing: {
+          128: "32rem",
+          144: "36rem",
+        },
+        borderRadius: {
+          "4xl": "2rem",
+        },
+    
+      colors: {
+        beMain: "#FDF6EA",
+        beLight: "#F5E9D4",
+        xanhMain: "#838C48",
       },
     },
-    colors: {
-      beMain: "#FDF6EA",
-      beLight: "#F5E9D4",
-      xanhMain: "#838C48",
-    },
+
   },
   plugins: [],
 };
