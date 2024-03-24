61244ef27ca2c8e70e1c6cb68a90b8bb19e1b762
trandangquang <52559661+trandangquang@users.noreply.github.com>
2024-03-24 15:50:54 +0700

    Feat/tsp 11 (#13)

--- Client/src/js/home.js
+++ Client/src/js/home.js
@@ -82,3 +82,24 @@ function animateNumber(
   
   window.addEventListener('resize', updateButtons);
   updateButtons();
+
+
+   const swiper = new Swiper('.mySwiper', {
+     // Default parameters
+     slidesPerView: 1,
+     // Responsive breakpoints
+     breakpoints: {
+       // when window width is >= 320px
+       320: {
+         slidesPerView: 1,
+       },
+       // when window width is >= 480px
+       768: {
+         slidesPerView: 2,
+       },
+       // when window width is >= 640px
+       1024: {
+         slidesPerView: 3,
+       },
+     },
+   });
