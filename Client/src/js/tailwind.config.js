376576bde7a78c82a7520fba959d53b985c342eb
SelinaHoHo <ngocthanhnbk123@gmail.com>
2024-03-24 18:38:22 +0700

    feat(TSP-10): update courses section

--- Client/src/js/tailwind.config.js
+++ Client/src/js/tailwind.config.js
@@ -1,32 +1,32 @@
 tailwind.config = {
-    theme: {
+  theme: {
+    extend: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+      },
+      fontFamily: {
+        open: ["Open Sans", "sans-serif"],
+        serif: ["Raleway", "sans-serif"],
+      },
       extend: {
-        screens: {
-          sm: "640px",
-          md: "768px",
-          lg: "1024px",
+        spacing: {
+          128: "32rem",
+          144: "36rem",
         },
-        fontFamily: {
-          open: ["Open Sans", "sans-serif"],
-          serif: ["Raleway", "sans-serif"],
+        borderRadius: {
+          "4xl": "2rem",
         },
-        extend: {
-          spacing: {
-            128: "32rem",
-            144: "36rem",
-          },
-          borderRadius: {
-            "4xl": "2rem",
-          },
-        },
-        colors: {
-          beMain: "#FDF6EA",
-          beLight: "#F5E9D4",
-          xanhMain: "#838C48",
-        },
-        backgroundImage: {
-          'inTouch': "url('../src/assests/images/newsletter-parallax.png')",
-        }
+      },
+      colors: {
+        beMain: "#FDF6EA",
+        beLight: "#F5E9D4",
+        xanhMain: "#838C48",
+      },
+      backgroundImage: {
+        inTouch: "url('../src/assests/images/newsletter-parallax.png')",
       },
     },
-  }
+  },
+};
