eb9996cb406af36c7b558188c513a6b949cbdee4
SelinaHoHo <ngocthanhnbk123@gmail.com>
2024-03-25 22:44:42 +0700

    feat(TSP-6): create teacher page

--- Express/src/server.js
+++ Express/src/server.js
@@ -57,6 +57,11 @@ server.get("/courses", (req, res, next) => {
   });
 });
 
+server.get("/teacher", (res) => {
+  let data = router.db.get("teacher").value();
+  res.json(data);
+});
+
 server.get("/search/courses", (req, res, next) => {
   const queryParams = req.query;
   let data = router.db.get("courses").value();
